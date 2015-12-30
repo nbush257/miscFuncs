b59b2c7e69276bdbc8c6b5ac92b87235d6e5eed1
nbush257 <nbush257@gmail.com>
2015-12-30 11:14:40 -0600

    added shadeVector which shades during contact periods

---
+++ shadeVector.m
@@ -0,0 +1,19 @@
+function shadeVector(C)
+%% function shadeVector(C)
+% This function takes a binary vector and shades X values where the binary vector is true. 
+% Designed to be used to take a contact binary and shade during contact
+% periods. 
+
+ax = gca;
+if ~islogical(C)
+    C = logical(C);
+end
+
+C(1) = 0;
+C(end) = 0;
+cStart = find(diff(C)==1)+1;
+cEnd = find(diff(C)==-1);
+ho
+for ii = 1:length(cStart)
+    fill([cStart(ii) cStart(ii) cEnd(ii) cEnd(ii)],[ax.YLim(1) ax.YLim(2) ax.YLim(2) ax.YLim(1)],'k','facealpha',.5,'edgecolor','none')
+end
