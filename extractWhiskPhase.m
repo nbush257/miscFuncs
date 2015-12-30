7d97d1fd0a06208ee529dd2c32a54732d36b76d1
nbush257 <nbush257@gmail.com>
2015-12-30 12:34:38 -0600

    debugged whisking phase extraction. Also made it so the output is aligned to the input theta regardless of nangaps

--- extractWhiskPhase.m
+++ extractWhiskPhase.m
@@ -1,4 +1,4 @@
-function [phase,amplitude] = extractWhiskPhase(theta,sr)
+function [phaseOut,amplitudeOut] = extractWhiskPhase(theta,sr)
 %% function [phase,amplitude] = extractWhiskPhase(theta,sr)
 % Uses the hilbert transform to calculate whisking phase as introdecud by
 % (Hill 2011, Neuron)
@@ -6,7 +6,6 @@ function [phase,amplitude] = extractWhiskPhase(theta,sr)
 % If there are nans it interpolates over 50 ms. 
 
 %% deal with nans in the angle trace
-warning('May need a geree to radian conversion')
 if any(isnan(theta))
     interpolateWindwow = sr*.05; % interpolate over nan gaps that are less than 50 ms
     theta= InterpolateOverNans(theta,interpolateWindwow);
@@ -16,9 +15,9 @@ if any(isnan(theta))
     if any(isnan(theta))
         disp('Splitting the trace up into several cells based on nan gaps')
         nan_logical = ~isnan(theta);% find where not nan regions are
-        nan_logical(1) = 0;nan_logical(end) = 0; % This is a workaround
-        nanStart = find(diff(nan_logical)==1)+1;
-        nanEnd = find(diff(nan_logical)==-1);
+        nan_logical = [0 nan_logical  0]; % This allows the find(diff()) to include the first and last indices
+        nanStart = find(diff(nan_logical)==1);
+        nanEnd = find(diff(nan_logical)==-1)-1;
         % Put each continuous stretch of non-nans into a cell
         for ii = 1:length(nanStart)
                 theta2{ii} = theta(nanStart(ii):nanEnd(ii));
@@ -34,7 +33,7 @@ end
 for ii = 1:length(theta2)
     theta2{ii} = bwfilt(theta2{ii},sr,6,60);
 end
-
+%% Apply Hilbert transform and convert into polar coordinates
 for ii = 1:length(theta2)
     
     H = hilbert(theta2{ii});
@@ -45,9 +44,12 @@ for ii = 1:length(theta2)
     [phase{ii},amplitude{ii}] = cart2pol(H(:,1),H(:,2));
 end
 
-% put the phase and amplitudes back into the original time
-phaseOut = nan(length(theta),1);
-amplitudeOut = nan(length(theta,1));
+%% put the phase and amplitudes back into the original time
+% init the vectors as nan the size of the original theta
+phaseOut = nan(size(theta));
+amplitudeOut = nan(size(theta));
+
+% loop over the cells and put the phase and amp values in the right place.
 for ii = 1:length(nanStart)
     phaseOut(nanStart(ii):nanEnd(ii)) = phase{ii};
     amplitudeOut(nanStart(ii):nanEnd(ii)) = amplitude{ii};
