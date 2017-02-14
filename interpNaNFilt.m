d9e48ffb04a1a8230dff441420039928d6404a4a
nbush257 <nbush257@gmail.com>
2017-02-14 12:41:32 -0600

    made interpNaNFilt capable of accepting a matrix

--- interpNaNFilt.m
+++ interpNaNFilt.m
@@ -1,13 +1,13 @@
-function xOut = interpNaNFilt (x,sr,lp,varargin)
-%% function xOut = interpNaNFilt (x,sr,lp,[winsize])
+function xOut = interpNaNFilt (X,sr,lp,varargin)
+%% function xOut = interpNaNFilt (X,sr,lp,[winsize])
 % ==================================================
 % Applies an acausal lowpass filter to a vector x which contains NaNs. This
 % is particularly useful if you have many whisks intervening with NaN
-% sections. It will also fill in NaN gaps if desired (defaults to fill gaps 
+% sections. It will also fill in NaN gaps if desired (defaults to fill gaps
 % less than 10 ms)
 % ===================================================
 %   INPUTS:
-%           x - a vector of some signal you want to filter
+%           x - a matrix of some signal you want to filter
 %           sr - sampling rate in Hz.
 %           lp - lowpass threshold to filter at
 %           [winsize] - size of window at which you want to fill small gaps.
@@ -25,37 +25,42 @@ optargs(1:numvargs) = varargin;
 [winsize] = optargs{:};
 
 %%
-winsize = sr/1000*winsize; % convert winsize into samples
-
-if ~isvector(x)
-    error('Input is not a vector')
-end
-% make x a column vector
-x = x(:);
-
-% remove small gaps if desired. Default is 10 ms. Skips if the gap is zero
-if winsize >0
-    x = InterpolateOverNans(x,winsize);
+winsize = round(sr/1000*winsize); % convert winsize into samples
+% check to see if matrix has time along length of column (i.e., each row is
+% a timepoint)
+if size(X,2)>size(X,1)
+    error('Stimulus matrix is likely not oriented with time along length of column.')
 end
 
-%% find not NaN segments to filter
-xOut = NaN(size(x));
-
-notNan = ~isnan(x); % first find where it is not a NaN
-notNaN = [0; notNan; 0]; % add these for easier diffing (and force first frame to be a start)
-difNaN = diff(notNaN);
-starts = find(difNaN == 1);  % mark where all nonNaN segments START
-stops = find(difNaN == -1) - 1; % mark where all nonNaN segments END
-nsegs = length(starts); % how many nonNaN segments are there
-
-%% Loop over not nan segments and filter them
-for ii = 1:nsegs
-    inds = starts(ii):stops(ii);
+% loop over all column vectors
+for ii = 1:size(X,2)
+    % store column vector
+    x_vec = X(:,ii);
     
-    % skip the filtering if the segment is less than 6 points long
-    if length(inds)<=6
-        continue
+    % remove small gaps if desired. Default is 10 ms. Skips if the gap is zero
+    if winsize >0
+        x_vec = InterpolateOverNans(x_vec,winsize);
     end
     
-    xOut(inds) = bwfilt(x(inds),sr,0,lp);% 
+    %% find not NaN segments to filter
+    xOut(:,ii) = NaN(size(x_vec));
+    
+    notNan = ~isnan(x_vec); % first find where it is not a NaN
+    notNaN = [0; notNan; 0]; % add these for easier diffing (and force first frame to be a start)
+    difNaN = diff(notNaN);
+    starts = find(difNaN == 1);  % mark where all nonNaN segments START
+    stops = find(difNaN == -1) - 1; % mark where all nonNaN segments END
+    nsegs = length(starts); % how many nonNaN segments are there
+    
+    %% Loop over not nan segments and filter them
+    for jj = 1:nsegs
+        inds = starts(jj):stops(jj);
+        
+        % skip the filtering if the segment is less than 6 points long
+        if length(inds)<=6
+            continue
+        end
+        
+        xOut(inds,ii) = bwfilt(x_vec(inds),sr,0,lp);%
+    end
 end
