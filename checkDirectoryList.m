1a467bdb2c2992ae8bcd5bda76f11f54300fd6b4
nbush257 <nbush257@gmail.com>
2017-06-16 09:42:46 -0500

    checkDirectoryList now can take an optional argument to define the regexp pattern

--- checkDirectoryList.m
+++ checkDirectoryList.m
@@ -1,7 +1,13 @@
-function [extra, found] = checkDirectoryList(spec,list)
+function [extra, found] = checkDirectoryList(spec,list,varargin)
 % use this function to test if the directory has all the data files we
 % want, and no extra. This will need to be modified a little for different
 % use cases.
+%%
+narginchk(2,3);
+if length(varargin)==1
+    regex_exp = '^rat\d{4}_\d{2}_[A-Z]{3}\d\d_VG_[A-Z]\d';
+end
+
 %%
 d = dir(spec);
 d_list = {d.name};
@@ -16,7 +22,7 @@ end
 %% see if all there are any data files that should not be in the directory that are
 % no trial suffix
 for ii = 1:length(d_list)
-    token = regexp(d_list{ii},'^rat\d{4}_\d{2}_[A-Z]{3}\d\d_VG_[A-Z]\d','match');
+    token = regexp(d_list{ii},regex_exp,'match');
     extra(ii) = ~any(~cellfun(@isempty,strfind(list,token)));
 end
 %% With trial suffix
