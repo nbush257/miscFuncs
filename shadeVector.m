9c99797a8f15217c6f8fdd873786728e6148499a
nbush257 <nbush257@gmail.com>
2017-07-31 12:41:19 -0500

    shadeVector now accepts a color argument

--- shadeVector.m
+++ shadeVector.m
@@ -1,11 +1,18 @@
-function shadeVector(C)
-%% function shadeVector(C)
+function shadeVector(C,varargin)
+%% function shadeVector(C,[color])
 % =======================
 % This function takes a binary vector and shades X values where the binary vector is true. 
 % Designed to be used to take a contact binary and shade during contact
-% periods. 
+% periods. If a seconfd argument is passed, the color is defined.
 % ======================
 % NEB 2016_06
+%%
+if length(varargin)==1
+    fill_color = varargin{1};
+else
+    fill_color = 'k';
+end
+
 
 ax = gca;
 C(isnan(C)) = 0;
@@ -53,6 +60,6 @@ else %% if the input is a vector, convert to diffs then shade
     cEnd = find(diff(C)==-1)-1;
     
     for ii = 1:length(cStart)
-        fill([cStart(ii) cStart(ii) cEnd(ii) cEnd(ii)],[ax.YLim(1) ax.YLim(2) ax.YLim(2) ax.YLim(1)],'k','facealpha',.2,'edgecolor','none')
+        fill([cStart(ii) cStart(ii) cEnd(ii) cEnd(ii)],[ax.YLim(1) ax.YLim(2) ax.YLim(2) ax.YLim(1)],fill_color,'facealpha',.2,'edgecolor','none')
     end
 end
